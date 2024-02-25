7a05d20dfa6402707b88e7e42ddb8a24c7fc0422
slowlorizz <slowloris.tech@gmail.com>
2024-02-25 19:51:21 +0100

    update

--- installers/Install-Dependencies.sh
+++ installers/Install-Dependencies.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
 
-LIBTINS_DIR="../libs/libtins"
-
-if [[ "${PWD}" =~ ^.*\/installers$ ]]
-then
-    cwd="${PWD}"
-
-    git clone https://github.com/mfontanini/libtins.git $LIBTINS_DIR
-
-    apt-get install -y libpcap-dev libssl-dev cmake
-
-    mkdir "${LIBTINS_DIR}/build"
-    cd "${LIBTINS_DIR}/build"
-
-    cmake ../ -DLIBTINS_ENABLE_CXX11=1
-    #make
-
-    make install
-    ldconfig
-
-    cd $cwd
-else
-    printf "NOT ON INSTALLER PATH\n"
-fi
+sudo apt-get install -y libtins-dev libpcap-dev libssl-dev cmake
+sudo ldconfig
--- main.cpp
+++ main.cpp
@@ -1,29 +1,46 @@
-// For Windows support, is checked on compile-time
-#if defined (__WIN32__)
-    #ifndef TINS_STATIC
-    #define TINS_STATIC
-    #endif //TINS_STATIC
-#endif
-
-#if defined (_WIN32)
-    #ifndef TINS_STATIC
-    #define TINS_STATIC
-    #endif //TINS_STATIC
-#endif
-
-#if defined (_MSC_VER)
-    #ifndef TINS_STATIC
-    #define TINS_STATIC
-    #endif //TINS_STATIC
-#endif
-
-#if defined (__CYGWIN32__)
-    #ifndef TINS_STATIC
-    #define TINS_STATIC
-    #endif //TINS_STATIC
-#endif
-//------------------------------------------------------------
-
 #include <tins/tins.h>
+#include <bits/stdc++.h>
+#include <csignal>
 
-// "C:\Program Files\CMake\bin\cmake.exe" "../" -DPCAP_ROOT_DIR="C:\Users\loris\OneDrive\Dokumente\GitHub\cpp-ProbeRequests\libs\WinPcap\WpdPack" -DLIBTINS_ENABLE_WPA2=1 -DLIBTINS_BUILD_SHARED=0
+using namespace std;
+using namespace Tins;
+
+bool loop = true;
+
+void raiseFlag(int p) {
+    loop = false;
+}
+
+bool callback(const PDU& pdu) {
+    const Dot11* dot11 = pdu.find_pdu<Dot11>();
+
+    if(!dot11) return true;
+
+    const Dot11ProbeRequest* probe = dot11->find_pdu<Dot11ProbeRequest>();
+
+    if(!probe) return true;
+
+    string mac = probe->addr2().to_string();
+    string ssid = probe->ssid();
+    int signal = pdu.rfind_pdu<RadioTap>().dbm_signal();
+
+    cout << "(" << signal << "dBm)  [" << mac << "]\t-->\t" << ssid << endl;
+
+    return loop;
+}
+
+int main(int argc, char* argv[]) {
+    if(argc != 2) {
+        cout << "Usage: " << argv[0] << " <interface>" << endl;
+        return 1;
+    }
+
+    signal(SIGINT, raiseFlag);
+
+    SnifferConfiguration config;
+    config.set_filter("wlan");
+    config.set_promisc_mode(true);
+
+    Sniffer sniffer(argv[1], config);
+    sniffer.sniff_loop(callback);
+}
